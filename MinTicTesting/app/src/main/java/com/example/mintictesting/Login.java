c0ef884023f71953d24fbccc70d2c6c12aa6917d
Crist-C <castro.cristiank@gmail.com>
2021-11-13 02:40:26 -0500

    Se crea la primera interfaz, la de login. Se dejan de manera estandar la paleta de colores.

--- MinTicTesting/app/src/main/java/com/example/mintictesting/Login.java
+++ MinTicTesting/app/src/main/java/com/example/mintictesting/Login.java
@@ -1,13 +1,20 @@
 package com.example.mintictesting;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Login extends AppCompatActivity {
 
+    TextInputEditText inputText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+
     }
 }
