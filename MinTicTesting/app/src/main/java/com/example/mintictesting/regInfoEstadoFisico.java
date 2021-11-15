785467f33c6b9e90492bd2f7a7a09d63d59ebb38
mikemontsk8 <90118418+mikemontsk8@users.noreply.github.com>
2021-11-15 13:17:37 -0500

    Empece a hacer el diseño de informacion de registro de estado fisico.

---
+++ MinTicTesting/app/src/main/java/com/example/mintictesting/regInfoEstadoFisico.java
@@ -0,0 +1,13 @@
+package com.example.mintictesting;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class regInfoEstadoFisico extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg_info_estado_fisico);
+    }
+}
