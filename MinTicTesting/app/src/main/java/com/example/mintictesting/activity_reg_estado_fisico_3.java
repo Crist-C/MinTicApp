ac9c00190557d9ebfe885fa4d0d5dcec880c0175
mikemontsk8 <90118418+mikemontsk8@users.noreply.github.com>
2021-11-17 11:41:23 -0500

    continuo con las demas actividades.

---
+++ MinTicTesting/app/src/main/java/com/example/mintictesting/activity_reg_estado_fisico_3.java
@@ -0,0 +1,14 @@
+package com.example.mintictesting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_reg_estado_fisico_3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reg_estado_fisico3);
+    }
+}
