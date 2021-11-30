bc0163548a54f93612eb98456dd13ca5b7a8d86d
luchoml <luismale@gmail.com>
2021-11-29 21:08:40 -0500

    Diana Rodriguez realize la interfaz de pistas.

---
+++ MinTicTesting/app/src/main/java/com/example/mintictesting/PistasRV.java
@@ -0,0 +1,14 @@
+package com.example.mintictesting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PistasRV extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pistasrv);
+    }
+}
